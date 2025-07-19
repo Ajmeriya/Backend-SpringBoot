aa2817d130d6776e1af975fa3dc5aef58cfd465b
Harsh Ajmeriya <ajmeriyaharsh932@gmail.com>
2025-07-19 16:25:57 +0530

    project curd complate

--- 04-spring-boot-rest-curd/05-curd-project/01-spring-boot-rest-curd-project/src/main/java/com/example/spring_rest_curd_project/SpringRestCurdProjectApplication.java
+++ 04-spring-boot-rest-curd/05-curd-project/01-spring-boot-rest-curd-project/src/main/java/com/example/spring_rest_curd_project/SpringRestCurdProjectApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.spring_rest_curd_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringRestCurdProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringRestCurdProjectApplication.class, args);
 	}
 
 }
--- 04-spring-boot-rest-curd/05-curd-project/01-spring-boot-rest-curd-project/src/test/java/com/example/spring_rest_curd_project/SpringRestCurdProjectApplicationTests.java
+++ 04-spring-boot-rest-curd/05-curd-project/01-spring-boot-rest-curd-project/src/test/java/com/example/spring_rest_curd_project/SpringRestCurdProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.spring_rest_curd_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringRestCurdProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
