b53a63bf4e1dad18d2829d13d1b430efeeadf0da
Harsh Ajmeriya <ajmeriyaharsh932@gmail.com>
2025-07-21 23:19:44 +0530

    start of rest security

---
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security/src/main/java/com/example/spring_rest_curd_project/SpringRestCurdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_rest_curd_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCurdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCurdProjectApplication.class, args);
+	}
+
+}
---
+++ 05-spring-boot-rest-security/01-spring-boot-rest-security/src/main/java/com/example/spring_rest_curd_project/rest/EmployeeRestController.java
@@ -0,0 +1,108 @@
+package com.example.spring_rest_curd_project.rest;
+
+import com.example.spring_rest_curd_project.dao.EmployeeDAO;
+import com.example.spring_rest_curd_project.dao.EmployeeDAOJpaImpl;
+import com.example.spring_rest_curd_project.entity.Employee;
+import com.example.spring_rest_curd_project.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+    private ObjectMapper objectMapper;
+
+    //quick and dirty : injection employee dao
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper objectMapper)
+    {
+        employeeService=theEmployeeService;
+        objectMapper=this.objectMapper;
+    }
+
+    //expose "/employee" and retun a list employee
+
+    @GetMapping("/employees")
+    public List<Employee> findAll()
+    {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId)
+    {
+        Employee theEmployee=employeeService.findById(employeeId);
+
+        if(theEmployee==null)
+        {
+            throw new RuntimeException("Employee Id not found -" + employeeId);
+        }
+        else
+        {
+            return theEmployee;
+        }
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee)
+    {
+        theEmployee.setId(0);
+
+        Employee tempEmployee= employeeService.save(theEmployee);
+
+        return tempEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee uodateEmployee(@RequestBody Employee theEmployee)
+    {
+        Employee tempEmployee=employeeService.save(theEmployee);
+
+        return tempEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void delateEmployee(@PathVariable("employeeId") int theId)
+    {
+        employeeService.deleteById(theId);
+    }
+
+    @PatchMapping("/employee/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId,@RequestBody Map<String,Object> PatchPayLoad)
+    {
+        Employee tempEmployee=employeeService.findById(employeeId);
+
+        if(tempEmployee==null)
+        {
+            throw new RuntimeException("employee id not found-" + employeeId);
+        }
+        if(PatchPayLoad.containsKey("id"))
+        {
+            throw new RuntimeException("you are not allow to chnage id");
+        }
+        Employee PatchedEmployee =apply(PatchPayLoad,tempEmployee);
+
+        Employee dbEmployee=employeeService.save(PatchedEmployee);
+
+        return dbEmployee;
+
+    }
+
+    private Employee apply(Map<String, Object> patchPayLoad, Employee tempEmployee) {
+
+        ObjectNode employeeNdoe=objectMapper.convertValue(tempEmployee,ObjectNode.class);
+
+        ObjectNode patchNode=objectMapper.convertValue(patchPayLoad,ObjectNode.class);
+
+        employeeNdoe.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNdoe,Employee.class);
+    }
+
+
+}
